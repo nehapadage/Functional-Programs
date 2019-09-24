7135244eb6eab41dcf54b5587058725f2854f6fc
tekina.makin <tekina.makin@gmail.com>
2019-09-24 18:57:08 +0530

    functional programs added

--- add.js
+++
@@ -1,5 +0,0 @@
-var a,b,c;
-a=5;
-b=10;
-c=a+b;
-console.log(c);
--- utility/utility.js
+++ utility/utility.js
@@ -16,38 +16,7 @@ module.exports={
     },
 
 
-  anagram(s1,s2)
-{
-var n1=s1.length;
-var n2=s2.length;
-var flag=false;
-    if(n1==n2)
-    {
-    s1.sort();
-    s2.sort();
-      for(var i=0;i<n1;i++)
-      {
-        if(s1[i]==s2[i]) 
-        {
-        flag=true;
-        }
-      }
-    }
-    else
-    {
-    flag=false;
-    }
-
-if(flag==true)
-{
-console.log("Strings are anagram.");
-}
-else
-{
-console.log("Strings are not anagram.");
-}
-},
-
+  
 
 coin(nflip)
         {
